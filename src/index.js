604ce6f082078c8e193218862d169a48b5483240
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-12-11 20:02:28 +0600

    implement error handling utilities

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: ".env"
+  path: ".env",
 });
-connectDB();
+
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    })
+  )
+  .catch((error) => console.error("MongoDB connection failed:", error));

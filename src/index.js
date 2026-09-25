dbad1ba9023285cdc21e1feedebd88eae24ccbda
Wasifur Rahman Efaz <rahmanefazwasifur@gmail.com>
2026-09-25 16:49:38 +0600

    restructure index.js to enhance express app setup and routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-// src/index.js
-
 import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import connectDB from "./db/index.js";
-import app from "./expressApp.js";
+import userRoutes from "./routes/user.routes.js";
+import spendingRecordRoutes from "./routes/spendingRecord.routes.js";
+import updateDailyRecord from "./routes/dailyRecords.routes.js";
+import transactionRoutes from "./routes/transaction.routes.js";
 
 dotenv.config();
 
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      // "https://your-frontend.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "500kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth/users", userRoutes);
+app.use("/api/v1/spending-records", spendingRecordRoutes);
+app.use("/api/v1/updateDailyRecord", updateDailyRecord);
+app.use("/api/v1/transactions", transactionRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Finance Management API is running",
+  });
+});
+
+// Connect to MongoDB
 await connectDB();
 
 export default app;

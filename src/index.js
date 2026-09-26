ad2545629710ec2277cbf86b3230bef9566f34c3
Wasifur Rahman Efaz <rahmanefazwasifur@gmail.com>
2026-09-26 17:40:37 +0600

    added production frontend url

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      // "https://your-frontend.vercel.app",
+      "https://finx-fawn.vercel.app",
     ],
     credentials: true,
   }),
